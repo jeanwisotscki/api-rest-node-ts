0c6bbce88bf2a19050d2f9670dfd42a22f56906d
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-10-19 10:27:01 -0300

    feat: lib yup

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,16 +1,14 @@
 import { Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { CidadesController } from "../controllers";
+
 const router = Router();
 
 router.get("/", (_, res) => {
   return res.send("Fala, get!");
 });
 
-router.post("/post", (req, res) => {
-  console.log(req.body);
-
-  return res.status(StatusCodes.OK).json(req.body);
-});
+router.post("/cidades", CidadesController.create);
 
 export { router };
