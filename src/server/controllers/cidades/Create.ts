29d6298309b37223379e65a1dca8098f641282e1
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-10-19 11:14:01 -0300

    feat: validações e tradução yup adicionados

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,24 +4,31 @@ import * as yup from "yup";
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.SchemaOf<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
   } catch (err) {
     const yupError = err as yup.ValidationError;
-    console.log(yupError.message);
+    const errors: Record<string, string> = {};
 
-    return res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ errors: { default: yupError.message } });
+    yupError.inner.forEach((error) => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   return res.send(validatedData);
