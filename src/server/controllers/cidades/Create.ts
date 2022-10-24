961e35976f4a4a5cb5199e8d569d17374e50e7ce
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-10-24 08:13:23 -0300

    refactor: metodo create

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 import { validation } from "../../shared/middleware";
 
 interface ICidade {
   nome: string;
-  estado: string;
 }
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICidade>(
     yup.object().shape({
       nome: yup.string().required().min(3),
-      estado: yup.string().required().min(3),
     })
   ),
 }));
@@ -20,5 +19,5 @@ export const createValidation = validation((getSchema) => ({
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   console.log(req.body);
 
-  return res.send("Criado");
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado");
 };
