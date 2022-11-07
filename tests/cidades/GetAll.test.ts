e62cb10bc7e0ccf49cba6ea1841af543442dc374
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-11-07 09:15:52 -0300

    refactor: ajustes nos testes cidades - get all

--- tests/cidades/GetAll.test.ts
+++ tests/cidades/GetAll.test.ts
@@ -4,8 +4,15 @@ import { StatusCodes } from "http-status-codes";
 describe("cidades - GetAll", () => {
   //
   it("Busca todos os registros", async () => {
-    const res1 = await testServer.get("/cidades");
+    const res1 = await testServer.post("/cidades").send({
+      nome: "Cidade de teste",
+    });
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
 
-    expect(res1.statusCode).toEqual(StatusCodes.OK);
+    const resGetAll = await testServer.get("/cidades").send();
+
+    expect(Number(resGetAll.header["x-total-count"])).toBeGreaterThan(0);
+    expect(resGetAll.statusCode).toEqual(StatusCodes.OK);
+    expect(resGetAll.body.length).toBeGreaterThan(0);
   });
 });
