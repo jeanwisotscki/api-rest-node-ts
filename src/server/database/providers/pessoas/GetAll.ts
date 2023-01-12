98ccb7aec0d4755b8996493931cdfb8c534e8213
Jean Wisotscki <jean1wisotscki@gmail.com>
2023-01-12 11:09:02 -0300

    new: providers de pessoas

---
+++ src/server/database/providers/pessoas/GetAll.ts
@@ -0,0 +1,22 @@
+import { Knex } from "../../knex";
+import { IPessoa } from "../../models";
+import { ETableNames } from "../../ETableNames";
+
+export const getAll = async (
+  page: number,
+  limit: number,
+  filter: string
+): Promise<IPessoa[] | Error> => {
+  try {
+    const result = await Knex(ETableNames.pessoa)
+      .select("*")
+      .where("nomeCompleto", "like", `%${filter}%`)
+      .offset((page - 1) * limit)
+      .limit(limit);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return new Error("Erro ao consultar os registros.");
+  }
+};
---
+++ src/server/database/providers/pessoas/index.ts
@@ -0,0 +1,15 @@
+import * as count from "./Count";
+import * as create from "./Create";
+import * as getAll from "./GetAll";
+import * as getById from "./GetById";
+import * as updateById from "./UpdateById";
+import * as deleteById from "./DeleteById";
+
+export const CidadesProvider = {
+  ...count,
+  ...create,
+  ...getAll,
+  ...getById,
+  ...updateById,
+  ...deleteById,
+};
